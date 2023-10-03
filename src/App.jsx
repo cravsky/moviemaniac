57ea29407f8dde204b6ab80bca6039822aee7887
cravsky <cravsky@gmail.com>
2023-10-03 15:43:20 +0200

    Initia commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Navbar />
+      <main>Main content</main>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+import fire from '../../assets/fire.png'
+import star from '../../assets/glowing-star.png'
+import party from '../../assets/partying-face.png'
+
+const Navbar = () => {
+    return (
+        <nav className='navbar'>
+            <h1>Movie Maniac</h1>
+            <div className="navbar_links">
+                <a href="">Popular
+                    <img src={fire} className="navbar_emoji" alt="fire emoji">
+                    </img>
+                </a>
+                <a href="">Top Rated
+                    <img src={star} className="navbar_emoji" alt="star emoji">
+                    </img>
+                </a>
+                <a href="">Upcoming
+                    <img src={party} className="navbar_emoji" alt="party emoji">
+                    </img>
+                </a>
+
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
