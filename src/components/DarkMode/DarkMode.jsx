698106bd18389848aa6ee0b0dca7449d2796eeb3
cravsky <cravsky@gmail.com>
2023-10-09 13:59:23 +0200

    Added dark-light mode switch

---
+++ src/components/DarkMode/DarkMode.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./DarkMode.css";
+import { ReactComponent as Sun } from "./Sun.svg";
+import { ReactComponent as Moon } from "./Moon.svg";
+
+const DarkMode = () => {
+    return (
+        <div className='dark_mode'>
+            <input
+                className='dark_mode_input'
+                type='checkbox'
+                id='darkmode-toggle'
+            />
+            <label className='dark_mode_label' htmlFor='darkmode-toggle'>
+                <Sun />
+                <Moon />
+            </label>
+        </div>
+    );
+};
+
+export default DarkMode;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import './Navbar.css'
+import DarkMode from '../DarkMode/DarkMode'
 import fire from '../../assets/fire.png'
 import star from '../../assets/glowing-star.png'
 import party from '../../assets/partying-face.png'
@@ -9,6 +11,7 @@ const Navbar = () => {
         <nav className='navbar'>
             <h1>Movie Maniac</h1>
             <div className="navbar_links">
+                <DarkMode />
                 <a href="">Popular
                     <img src={fire} className="navbar_emoji" alt="fire emoji">
                     </img>
